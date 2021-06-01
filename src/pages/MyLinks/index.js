a5644d0a30be55e52a62ebe6b1bc977f06079abc
Maria Soledade <maryaferreira94@gmail.com>
2021-06-01 00:02:07 -0300

    Criando o projeto, adicionando react-navigation e drawer-navigation, criando componente Home e MyLinks, configurando rotas

---
+++ src/pages/MyLinks/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+ Text, View
+}from 'react-native';
+
+export default function MyLinks(){
+  return(
+    <View>
+      <Text>Página Links</Text>
+    </View>
+  )
+}
