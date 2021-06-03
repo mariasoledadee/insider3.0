23ffc62067e946666950a20002ebed66f961de3c
Maria Soledade <maryaferreira94@gmail.com>
2021-06-02 23:42:39 -0300

    Configurando api

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,12 @@
-//efab5a48364fc665c5c4b28e93701217f6d2532b
+import axios from 'axios';
+
+export const key = 'efab5a48364fc665c5c4b28e93701217f6d2532b';
+
+const api = axios.create({
+  baseURL: 'https://api-ssl.bitly.com/v4',
+  headers:{
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${key}`
+  }
+})
+export default api;
