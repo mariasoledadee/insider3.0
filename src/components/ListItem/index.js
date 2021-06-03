25671bebf1ff28032e1f53101b25cfc94cd72ae2
Maria Soledade <maryaferreira94@gmail.com>
2021-06-03 00:40:49 -0300

    Estilizando a parte de meus links

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+
+import {Feather} from '@expo/vector-icons';
+
+import {ContainerButton, Link} from './style';
 
 export default function ListItem(){
   return(
     <View>
-      <Text>Cansei</Text>
+      <ContainerButton activeOpacity={0.9} onPress={() => alert("Testando")}>
+        <Feather
+         name="link"
+         color="#fff"
+         size={24}
+        />
+        <Link numberOfLines={1}>htpps://youtube.com</Link>
+      </ContainerButton>
     </View>
   )
 }
--- src/components/ListItem/style.js
+++ src/components/ListItem/style.js
@@ -1,1 +1,14 @@
 import styled from 'styled-components/native';
+export const ContainerButton = styled.TouchableOpacity`
+ flex-direction: row;
+ background-color: rgba(255,255,255,0.21);
+ margin: 7px 10px;
+ padding: 12px;
+ border-radius: 7px;
+`;
+export const Link = styled.Text`
+ color: #fff;
+ padding-left: 10px;
+ padding-right: 20px;
+ font-size: 18px;
+`;
